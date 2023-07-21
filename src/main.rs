3c9632453f7720649cf98d2986fd35d6a3d21855
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-21 10:08:50 -0700

    split functionality into a submodule convert

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,15 @@
-use anyhow::Result;
+mod cli;
+mod convert;
+mod io;
 use clap::Parser;
-use flate2::read::MultiGzDecoder;
-use gtftools::GtfReader;
-use std::{
-    fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
-};
-
-#[derive(Parser)]
-struct Cli {
-    #[clap(short, long)]
-    input: String,
-    #[clap(short, long)]
-    output: Option<String>,
-}
-
-fn match_output(output: Option<String>) -> Box<dyn Write> {
-    match output {
-        Some(x) => Box::new(File::create(x).unwrap()),
-        None => Box::new(std::io::stdout()),
-    }
-}
-
-fn match_input(input: String) -> Box<dyn BufRead> {
-    match input.ends_with(".gz") {
-        true => Box::new(BufReader::new(MultiGzDecoder::new(
-            File::open(input).unwrap(),
-        ))),
-        false => Box::new(BufReader::new(File::open(input).unwrap())),
-    }
-}
+use cli::Cli;
+use anyhow::Result;
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-
-    // build the input handle
-    let input_handle = match_input(args.input);
-
-    // build the output handle
-    let output_handle = match_output(args.output);
-
-    // initialize the writer
-    let mut writer = BufWriter::new(output_handle);
-
-    // read in gtf records and parse to json
-    let json_records = GtfReader::from_bufread(input_handle)
-        .filter_map(|x| x.ok())
-        .map(|x| serde_json::to_string(&x).unwrap());
-
-    // write newline delimited json to output handle
-    for j in json_records {
-        writeln!(writer, "{j}")?;
+    match args.command {
+        cli::Command::Convert { input, output } => {
+            convert::convert(&input, &output)
+        }
     }
-
-    Ok(())
 }
