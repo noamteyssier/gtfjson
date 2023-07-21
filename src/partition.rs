7cd6ca748725a4236d5b2cf251bb96e52d59b1ae
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-21 11:44:55 -0700

    cargo fmt

--- src/partition.rs
+++ src/partition.rs
@@ -1,7 +1,11 @@
-use std::{fs::{File, OpenOptions}, io::{BufReader, BufRead, BufWriter, Write}, collections::{HashMap, HashSet}};
+use crate::{io::match_output, types::FlatRecord};
 use anyhow::Result;
-use spinoff::{Spinner, spinners, Color, Streams};
-use crate::{types::FlatRecord, io::match_output};
+use spinoff::{spinners, Color, Spinner, Streams};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, BufWriter, Write},
+};
 
 fn load_file(filename: &str) -> Result<BufReader<File>> {
     Ok(File::open(filename).map(BufReader::new)?)
@@ -13,7 +17,6 @@ pub fn partition(
     output_dir: &str,
     max_open_files: usize,
 ) -> Result<()> {
-
     let mut output_dir = output_dir.to_string();
     if !output_dir.ends_with("/") {
         output_dir.push_str("/");
@@ -27,7 +30,12 @@ pub fn partition(
     let mut partitions = HashSet::new();
     let mut handle_map = HashMap::new();
     let mut num_records = 0;
-    let mut spinner = Spinner::new_with_stream(spinners::Aesthetic, "Mapping JSON", Color::Green, Streams::Stderr);
+    let mut spinner = Spinner::new_with_stream(
+        spinners::Aesthetic,
+        "Mapping JSON",
+        Color::Green,
+        Streams::Stderr,
+    );
     for line in input_handle.lines() {
         let line = line?;
         let record = serde_json::from_str::<FlatRecord>(&line)?;
@@ -53,9 +61,17 @@ pub fn partition(
         }
         num_records += 1;
         if num_records % 100000 == 0 {
-            spinner.update_text(format!("Identified {} total partitions across {} records", partitions.len(), num_records));
+            spinner.update_text(format!(
+                "Identified {} total partitions across {} records",
+                partitions.len(),
+                num_records
+            ));
         }
     }
-    spinner.stop_with_message(&format!("Identified {} total partitions across {} records", partitions.len(), num_records));
+    spinner.stop_with_message(&format!(
+        "Identified {} total partitions across {} records",
+        partitions.len(),
+        num_records
+    ));
     Ok(())
 }
